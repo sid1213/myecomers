cc011abdfc23d497a6f1ef0aee804323dfe851b5
sidharth1213 <siddharth.soni@cobold.in>
2023-04-22 23:02:03 +0530

    using Layout component from antd

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import SingleProduct from "./pages/SingleProduct";
 import Footer from "./components/Footer";
+import { Layout, Space } from "antd";
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id?" element={<SingleProduct />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<h1>404 not found</h1>} />
-      </Routes>
-      <Footer />
+      <Layout>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id?" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>404 not found</h1>} />
+        </Routes>
+        <Footer />
+      </Layout>
     </Router>
   );
 }
