a5e373bbea86262d4e96af1fd360b5220cd2a7a7
sidharth1213 <siddharth.soni@cobold.in>
2023-04-28 14:42:09 +0530

    removed checkbox from login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -64,14 +64,6 @@ const Login: React.FC = () => {
             <Input.Password />
           </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button type="primary" htmlType="submit">
               Submit
