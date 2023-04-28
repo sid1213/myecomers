5fcf4293cec3337d1621ccc8e26d2f07db996841
sidharth1213 <siddharth.soni@cobold.in>
2023-04-28 14:41:56 +0530

    imported types

--- src/store/productSlice.ts
+++ src/store/productSlice.ts
@@ -4,59 +4,17 @@ import {
   PayloadAction,
   combineReducers,
 } from "@reduxjs/toolkit";
+import {
+  AddAndRemoveItemState,
+  AllProductState,
+  SingleProductState,
+  UserOrderState,
+  cartDetails,
+  cartState,
+  userCartState,
+  userState,
+} from "../type/index";
 
-export interface ProductState {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-}
-export interface AllProductState {
-  items: ProductState[];
-  loading: boolean;
-  error: null | string;
-}
-export interface SingleProductState {
-  item: ProductState;
-  myloading: boolean;
-  myerror: null | string;
-}
-export interface cartDetails {
-  item: ProductState;
-  quantity: number;
-}
-export interface cartState {
-  AddedProducts: cartDetails[];
-  cartVolume: number;
-  totalAmt: number;
-}
-export interface AddAndRemoveItemState {
-  id: ProductState["id"];
-  operator: "minus" | "plus";
-}
-export interface userState {
-  user: {
-    name: string;
-    userName: string;
-    password: string;
-  };
-  userCart: cartDetails[];
-  userOrder: cartDetails[];
-}
-export interface userCartState {
-  userCart: cartDetails;
-  index: number;
-}
-export interface UserOrderState {
-  userCart: userState["userCart"];
-  index: number;
-}
 const initialState: AllProductState = {
   items: [],
   loading: true,
